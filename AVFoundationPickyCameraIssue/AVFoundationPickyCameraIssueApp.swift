4e5109b9af6a3816eedf30637ff4d2740a0fb0f7
dengweichao <dengweichao@hotmail.com>
2024-01-09 22:36:24 +0800

    Initial Commit

---
+++ AVFoundationPickyCameraIssue/AVFoundationPickyCameraIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVFoundationPickyCameraIssueApp.swift
+//  AVFoundationPickyCameraIssue
+//
+//  Created by Photon Juniper on 2024/1/9.
+//
+
+import SwiftUI
+
+@main
+struct AVFoundationPickyCameraIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
